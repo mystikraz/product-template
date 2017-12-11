f8bffb59b0ed8899fc4a2604e973708f55eebc9f
raj <royal_raj@outlook.com>
2017-12-11 17:15:55 +0545

    added product ui with animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import { routes } from './app.routes';
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule,MatToolbarModule,MatIconModule,MatCardModule,MatGridListModule,MatInputModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    routes,
+      MatButtonModule,
+      MatCheckboxModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatCardModule,
+      MatGridListModule,
+      MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
